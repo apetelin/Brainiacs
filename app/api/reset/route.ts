21d5244bdbba02f735919a27b6117c287bc556ba
Nikita <apz77@bk.ru>
2024-07-18 14:21:54 -0400

    fix reset

--- app/api/reset/route.ts
+++ app/api/reset/route.ts
@@ -1,11 +1,11 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextResponse } from 'next/server';
 import { prisma } from '@/lib/prisma';
 
 export async function GET(){
     try {
         const payments = await prisma.payment.deleteMany({
             where: {
-                userId: {
+                id: {
                     gt: 347,
                 },
             },
