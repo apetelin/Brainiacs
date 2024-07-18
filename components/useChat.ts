b2cadfa4ad6eccd8573a2572c2a4972870470fcd
Nikita <apz77@bk.ru>
2024-07-18 13:07:14 -0400

    Add chat and TTS

---
+++ components/useChat.ts
@@ -0,0 +1,38 @@
+import React from "react";
+import { sum } from "lodash";
+
+
+export function useChat() {
+    const [sending, setSending] = React.useState<string | boolean>(false);
+    const [conversationId, setConversationId] = React.useState<string|undefined>(undefined);
+    const [lastReply, setLastReply] = React.useState<string | undefined>(undefined);
+
+    function submit(userMessage: string) {
+        setSending(true);
+        const formData = new FormData();
+        formData.append("userMessage", JSON.stringify(userMessage));
+        if (conversationId) {
+            formData.append("conversationId", conversationId);
+        }
+        return fetch('api/chat', {
+            method: 'POST',
+            body: formData,
+        }).then((result) => {
+            if (result.ok) {
+                return result.json();
+            }
+            setSending("Error sending message");
+        }).then((json) => {
+            setSending(false);
+            if (json.response) {
+                setLastReply(json.response);
+            }
+            if (json.conversationId) {
+                setConversationId(json.conversationId);
+            }
+        })
+            .catch((e) => setSending("Error sending message"))
+    }
+
+    return {submitChat: submit, sendingChat: sending, conversationId, lastReply};
+}
