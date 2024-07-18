619dd51d81e2881d9cf8512796749473c6df3d4f
Ramyalakshmi <65313176+Ramya-sparkdatabox@users.noreply.github.com>
2024-07-18 14:02:47 -0400

    Update route.ts

--- app/api/reset/route.ts
+++ app/api/reset/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { prisma } from '@/lib/prisma';
 
-export async function DELETE(){
+export async function GET(){
     try {
         const payments = await prisma.payment.deleteMany({
             where: {
