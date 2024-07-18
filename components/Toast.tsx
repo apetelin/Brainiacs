5c3fb244224dce6c6cde15683e7d742abc881455
Nikita <apz77@bk.ru>
2024-07-18 12:10:52 -0400

    Change color

--- components/Toast.tsx
+++ components/Toast.tsx
@@ -11,7 +11,7 @@ interface ToastProps {
 
 export const Toast: React.FC<ToastProps> = ({ message, onAcknowledge, onApprove, onReject }) => {
     return (
-        <div className="fixed bottom-4 right-4 bg-gray-800 text-white p-4 rounded-lg shadow-lg max-w-sm">
+        <div className="fixed bottom-4 right-4 bg-blue-500 text-white p-4 rounded-lg shadow-lg max-w-sm">
             <p className="mb-2">{message}</p>
             <div className="flex justify-end space-x-2">
                 {onApprove && (
